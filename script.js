8d793c6d74a455bc6a766a4f9eeca9f809cc00df
andyreya <>
2021-06-08 13:52:24 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('button').addEventListener('click', clickHere)/*makes action occur when button is clicked*/
+
+function clickHere () {
+  alert(document.getElementById('input').value)
+}/*alerts window when text is put into textbox*/
