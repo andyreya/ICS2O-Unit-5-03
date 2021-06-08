c00a29d41f7617b61cc40275d4bd1740d6489652
andyreya <>
2021-06-08 14:01:52 +0000

    spaces

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.getElementById('button').addEventListener('click', clickHere)/*makes action occur when button is clicked*/
+document.getElementById('button').addEventListener('click', clickHere) /*  makes action occur when button is clicked */
 
 function clickHere () {
   alert(document.getElementById('input').value)
-}/*alerts window when text is put into textbox*/
+} /* alerts window when text is put into textbox  */
